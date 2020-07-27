5c2d58d9fd60ddec4155b9d98a9f194f6e710f67
wanglong-github <59468022+wanglong-github@users.noreply.github.com>
2020-07-27 19:20:28 +0800

    动态信息的类型进行汇总

--- flask_jianshu/app.py
+++ flask_jianshu/app.py
@@ -1,30 +1,96 @@
 import pymongo as pymongo
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
 def login():
-    #加载用户信息
-    client=pymongo.MongoClient('127.0.0.1')
-    db=client['jianshu']
-    ures=db['tabuser'].find({},{'_id':0})
-    ulist=[u for u in ures]
-    client.close()
-    return render_template('ulogin.html',list=ulist)
-def hello_world():
+    if request.method=='GET':
+        #加载用户信息
+        client=pymongo.MongoClient('127.0.0.1')
+        db=client['jianshu']
+        ures=db['tabuser'].find({},{'_id':0})
+        tli = db['tabtimeline'].find({}, {'slug': 1, '_id': 0})
+        timeusers=[u for u in tli]
+        ulist=[u for u in ures for x in timeusers if u['slug']==x['slug']]
+
+        client.close()
+        return render_template('ulogin.html',list=ulist)
+    else:
+        #接受slug
+        user_slug=request.form['uid']
+        return redirect(url_for('timeline',slug=user_slug))
+@app.route('/tline')
+def timeline():
+    #获取用户id
+    slug=request.args.get('slug')
     #链接数据库
     client=pymongo.MongoClient('127.0.0.1')
     db = client['jianshu']
-    tli = db['tabtimeline'].find({}, {'slug':1,'_id': 0})
-    userlist = [i['slug'] for i in tli]
-    x=[{
-        'name':'牛肉','value':500},
-        {'name':'牛肉','value':500},
-        {'name':'牛肉','value':500},
+
+    xli=db['tabtimeline'].find({'slug':slug},{'_id':0})
+    # print([i for i in xli])
+    #按照动态信息的类型进行汇总
+    xtype=[{'name':'发表评论','value':0},
+           {'name':'喜欢文章','value':0},
+           {'name':'赞赏文章','value': 0},
+           {'name':'发表文章','value': 0},
+           {'name':'关注用户','value': 0},
+           {'name':'关注专题','value': 0},
+           {'name':'点赞评论','value': 0},
+           {'name':'关注文集','value': 0},
     ]
-    return render_template('index.html',)
+    #得到动态信息
+    for i in xli:
+        #获取评论集合
+        for it in i['dydata']:
+            #判断字典是否有相关key，统计
+            if 'comment_note' in it.keys():
+                # print(f'发表评论:{len(it["comment_note"])}')
+                #修改对应类型数量的储存
+                for type in xtype:
+                    if type['name']=='发表评论':
+                        type['value']=len(it["comment_note"])
+                        break
+            elif 'like_note' in it.keys():
+                for type in xtype:
+                    if type['name']=='喜欢文章':
+                        type['value']=len(it["like_note"])
+                        break
+            elif 'reward_note' in it.keys():
+                for type in xtype:
+                    if type['name']=='赞赏文章':
+                        type['value']=len(it["reward_note"])
+                        break
+            elif 'share_note' in it.keys():
+                for type in xtype:
+                    if type['name']=='发表文章':
+                        type['value']=len(it["share_note"])
+                        break
+            elif 'like_user' in it.keys():
+                for type in xtype:
+                    if type['name']=='关注用户':
+                        type['value']=len(it["like_user"])
+                        break
+            elif 'like_collection' in it.keys():
+                for type in xtype:
+                    if type['name']=='关注专题':
+                        type['value']=len(it["like_collection"])
+                        break
+            elif 'like_comment' in it.keys():
+                for type in xtype:
+                    if type['name']=='点赞评论':
+                        type['value']=len(it["like_comment"])
+                        break
+            elif 'like_notebook' in it.keys():
+                for type in xtype:
+                    if type['name']=='关注文集':
+                        type['value']=len(it["like_notebook"])
+                        break
+    piekey=[k['name'] for k in xtype]
+    client.close()
+    return render_template('index.html',da=type,pkey=piekey)
 
 
 if __name__ == '__main__':
